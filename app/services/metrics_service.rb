7d16efe0f42d492d63a8762f36c8e5815ef79626
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2017-05-24 22:59:11 -0400

    WIP

--- app/services/metrics_service.rb
+++ app/services/metrics_service.rb
@@ -1,6 +1,12 @@
 class MetricsService
-    def MetricsService.average meal
+    def MetricsService.meal_average meal
         ratings = meal.ratings.to_a
         @avg = ratings.sum { |r| r.score } / ratings.size.to_f
     end
+
+    def MetricsService.meals_average meals
+        return nil
+    end
+    
+
 end
--- spec/services/metrics_service_spec.rb
+++ spec/services/metrics_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe MetricsService do
-    context 'average' do
+    context 'single meal average' do
         before do
             @meal = create :meal
             @rating1 = create(:rating, score: 0, meal: @meal)
@@ -10,10 +10,29 @@ describe MetricsService do
             @rating4 = create(:rating, score: 3, meal: @meal)
         end
         
-        it 'should calculate the average rating of a set of meals' do
-            result = MetricsService.average(@meal)
+        it 'should calculate the average rating of a meal' do
+            result = MetricsService.meal_average @meal
 
             expect(result).to eq(2.5)
         end
     end
+
+    context 'multi meal averge' do
+        before do
+            @meals = create_list(:meal)
+            @rating1 = create(:rating, score: 0, meal: @meals[0])
+            @rating2 = create(:rating, score: 3, meal: @meals[0])
+            @rating3 = create(:rating, score: 4, meal: @meals[0])
+            @rating4 = create(:rating, score: 3, meal: @meals[0])
+        
+            @rating5 = create(:rating, score: 4, meal: @meals[1])
+            @rating6 = create(:rating, score: 3, meal: @meals[1])
+            @rating7 = create(:rating, score: 1, meal: @meals[1])
+        end
+
+        it 'should calculate the average rating of a set of meals' do
+            result = MetricsService.meals_average @meals
+        end
+    end
+    
 end
