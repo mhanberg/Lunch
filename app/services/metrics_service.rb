661abf8ec85768f6bb03c1bb46e69722dcd93b6e
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2017-05-24 23:58:18 -0400

    MetricsService
     * methods are now instance methods
     * call by initializing an instance of MetricsService

--- app/services/metrics_service.rb
+++ app/services/metrics_service.rb
@@ -1,12 +1,15 @@
 class MetricsService
-    def MetricsService.meal_average meal
-        ratings = meal.ratings.to_a
-        @avg = ratings.sum { |r| r.score } / ratings.size.to_f
-    end
-
-    def MetricsService.meals_average meals
-        return nil
+    def initialize meals
+        @meals = [meals].flatten
     end
     
-
+    def meals_average
+        ratings = @meals.map { |meal| meal.ratings }.to_a.flatten
+        @avg = avg(ratings)
+    end
+    
+    private
+        def avg ratings
+            (ratings.sum { |r| r.score } / ratings.size.to_f).round(2)
+        end
 end
--- spec/services/metrics_service_spec.rb
+++ spec/services/metrics_service_spec.rb
@@ -1,25 +1,9 @@
 require 'rails_helper'
 
 describe MetricsService do
-    context 'single meal average' do
+    context 'meal average' do
         before do
-            @meal = create :meal
-            @rating1 = create(:rating, score: 0, meal: @meal)
-            @rating2 = create(:rating, score: 3, meal: @meal)
-            @rating3 = create(:rating, score: 4, meal: @meal)
-            @rating4 = create(:rating, score: 3, meal: @meal)
-        end
-        
-        it 'should calculate the average rating of a meal' do
-            result = MetricsService.meal_average @meal
-
-            expect(result).to eq(2.5)
-        end
-    end
-
-    context 'multi meal averge' do
-        before do
-            @meals = create_list(:meal)
+            @meals = create_list(:meal, 2)
             @rating1 = create(:rating, score: 0, meal: @meals[0])
             @rating2 = create(:rating, score: 3, meal: @meals[0])
             @rating3 = create(:rating, score: 4, meal: @meals[0])
@@ -29,10 +13,19 @@ describe MetricsService do
             @rating6 = create(:rating, score: 3, meal: @meals[1])
             @rating7 = create(:rating, score: 1, meal: @meals[1])
         end
+        
+        it 'should calculate the average rating of a single meal' do
+            result = MetricsService.new(@meals[0]).meals_average
+
+            expect(result).to eq(2.5)
+        end
 
         it 'should calculate the average rating of a set of meals' do
-            result = MetricsService.meals_average @meals
+            expected = (18.0 / 7.0).round(2)
+
+            result = MetricsService.new(@meals).meals_average
+
+            expect(result).to eq(expected)
         end
     end
-    
 end
