6e8f5baeda9db80ef318f7ef5e01a572b6062d28
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2017-07-23 18:29:15 -0400

    switched to single quoted strings

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -8,19 +8,19 @@ class Meal < ApplicationRecord
     meals.each do |meal|
       meal = meal.as_json
 
-      meal["title"] = meal["name"]
-      if meal["category"] == "Breakfast"
-        meal["start"] = meal["meal_date"].to_s + " 06:00:00"
-      elsif meal["category"] == "Lunch"
-        meal["start"] = meal["meal_date"].to_s + " 11:00:00"
+      meal['title'] = meal['name']
+      if meal['category'] == 'Breakfast'
+        meal['start'] = meal['meal_date'].to_s + ' 06:00:00'
+      elsif meal['category'] == 'Lunch'
+        meal['start'] = meal['meal_date'].to_s + ' 11:00:00'
       else
-        meal["start"] = meal["meal_date"].to_s + " 17:30:00"
+        meal['start'] = meal['meal_date'].to_s + ' 17:30:00'
       end
 
-      meal["rating"] = Rating.exists?(meal_id: meal["id"], user_id: user_id)
+      meal['rating'] = Rating.exists?(meal_id: meal['id'], user_id: user_id)
 
-      if meal["rating"]
-        meal["rating"] = Rating.where(meal_id: meal['id'], user_id: user_id).take.as_json
+      if meal['rating']
+        meal['rating'] = Rating.where(meal_id: meal['id'], user_id: user_id).take.as_json
       end
 
       meals_json << meal
