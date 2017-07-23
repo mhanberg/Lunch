638d5f81a8710151cb28e0fec694a898f2b85c5e
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2017-07-23 19:04:38 -0400

    set meal[‘start’] to result of conditional

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -11,15 +11,15 @@ class Meal < ApplicationRecord
     meals_json = []
     meals.each do |meal|
       meal = meal.as_json
-
       meal['title'] = meal['name']
-      if meal['category'] == 'Breakfast'
-        meal['start'] = meal['meal_date'].to_s + ' 06:00:00'
-      elsif meal['category'] == 'Lunch'
-        meal['start'] = meal['meal_date'].to_s + ' 11:00:00'
-      else
-        meal['start'] = meal['meal_date'].to_s + ' 17:30:00'
-      end
+      meal['start'] =
+        if meal['category'] == 'Breakfast'
+          meal['meal_date'].to_s + ' 06:00:00'
+        elsif meal['category'] == 'Lunch'
+          meal['meal_date'].to_s + ' 11:00:00'
+        else
+          meal['meal_date'].to_s + ' 17:30:00'
+        end
 
       meal['rating'] = Rating.exists?(meal_id: meal['id'], user_id: user_id)
 
@@ -31,5 +31,4 @@ class Meal < ApplicationRecord
     end
     @meals_json = meals_json
   end
-
 end
