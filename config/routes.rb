e39748458f6d723c3db07126c24951a7dfb2c69a
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2017-07-23 17:01:07 -0400

    Reorganized routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
-  resources :groups
   root 'welcome#index'
   get '/welcome', to: 'welcome#welcome'
 
+  resources :groups
+  post '/AddUserToGroup', to: 'groups#AddUserToGroup'
+  post '/DeleteUserFromGroup', to: 'groups#DeleteUserFromGroup'
+
   get '/signup', to: 'users#new'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   get '/console', to: 'sessions#console'
   get '/debug', to: 'sessions#debug'
-  post '/AddUserToGroup', to: 'groups#AddUserToGroup'
-  post '/DeleteUserFromGroup', to: 'groups#DeleteUserFromGroup'
+
+
   resources :users
   resources :meals
+  get '/calendar', to: 'meals#calendar'
   resources :ratings
   resources :metrics, only: :index
-  get '/calendar/', to: 'meals#calendar'
 end
