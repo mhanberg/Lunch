2b1e4056e2e80a4047c4a6587a49e7a6ed194ba5
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2017-09-20 15:00:44 -0400

    General clean up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,15 +11,23 @@ class UsersController < ApplicationController
 
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to lunch! Where your freshman 15 is born."
+      flash[:success] = 'Welcome to lunch! Where your freshman 15 is born.'
       redirect_to @user
     else
       render 'new'
     end
   end
 
-  private
-    def user_params
-      params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+  def default_group
+    unless current_user.update_attribute(:default_group, Group.find(params[:default_group_id]))
+      flash[:danger] = 'Failed to change default group'
     end
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+  end
 end
