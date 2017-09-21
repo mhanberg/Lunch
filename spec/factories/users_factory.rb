2c0426233aede6fc097c4b1b0046a650c59b047a
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2017-09-21 17:00:08 -0400

    fix test

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -1,8 +1,10 @@
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "default#{n}@example.com" }
-    first_name "Defaulty"
-    last_name "Defaultson"
-    password "password"
+    first_name 'Defaulty'
+    last_name 'Defaultson'
+    password 'password'
+
+    association :default_group, factory: :group
   end
 end
